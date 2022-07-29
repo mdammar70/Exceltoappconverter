506c52edfe852e0b886cb50922c36fa77a818315
mdAmmar <64901114+mdammar70@users.noreply.github.com>
2022-07-29 10:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
-st.subheader('Was the tutorial helpful?')
+
 
 ### --- LOAD DATAFRAME
 excel_file = 'Survey_Results.xlsx'
