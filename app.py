1193d8264c29342d9513fc0bb3e7051bfe3499cd
mdAmmar <64901114+mdammar70@users.noreply.github.com>
2022-07-29 10:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 from PIL import Image
 
-     st.set_page_config(page_title='Survey Results')
+st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
      st.subheader('Was the tutorial helpful?')
 
